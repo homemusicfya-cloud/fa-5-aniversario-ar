867e370f3063e17552805d9205e1fa6b42263d22
homemusicfya-cloud <homemusicfya@gmail.com>
2026-08-08 17:40:06 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const boton = document.getElementById("btnMusica");
+
+    boton.addEventListener("click", function () {
+
+        alert(
+            "La experiencia musical estará disponible aquí."
+        );
+
+    });
+
+});
